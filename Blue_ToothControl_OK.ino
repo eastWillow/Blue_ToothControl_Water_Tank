e23a92fad3f285383e010ae133b077dadca0ef65
eastWillow <bobgash2@gmail.com>
2015-07-12 01:56:22 +0800

    eight direction

--- Blue_ToothControl_OK.ino
+++ Blue_ToothControl_OK.ino
@@ -1,66 +1,103 @@
 #include <LBT.h>
 #include <LBTServer.h>
-  
 void setup()
 {
+  initialMotor();
   Serial.begin(9600);
   bool success = LBTServer.begin((uint8_t*)"WaterTank");
-  if( !success )
+  if ( !success )
   {
-      Serial.println("Cannot begin Bluetooth Server successfully");
-      // don't do anything else
-      while(true);
+    Serial.println("Cannot begin Bluetooth Server successfully");
+    // don't do anything else
+    while (true);
   }
   else
   {
-      Serial.println("Bluetooth Server begin successfully");
+    Serial.println("Bluetooth Server begin successfully");
   }
- pinMode(9, OUTPUT);
- pinMode(13,INPUT_PULLUP);
- LBTServer.setTimeout(30);
+  pinMode(9, OUTPUT);
+  pinMode(13, INPUT_PULLUP);
+  LBTServer.setTimeout(30);
 }
- 
+
 void loop()
 {
-  if(!LBTServer.connected())
+  if (!LBTServer.connected())
+  {
+    // waiting for Spp Client to connect
+    bool connected = LBTServer.accept(10);
+
+    if ( !connected )
     {
-       // waiting for Spp Client to connect
-       bool connected = LBTServer.accept(10);
- 
-       if( !connected )
-       {
-           Serial.println("No connection request yet");
-           // don't do anything else
-           while(!LBTServer.accept(1)){
-             Serial.println("No connection request yet");
-           }
-       }
-       else
-       {
-           Serial.println("Connected");
-       }
+      Serial.println("No connection request yet");
+      // don't do anything else
+      while (!LBTServer.accept(1)) {
+        Serial.println("No connection request yet");
+      }
     }
     else
     {
-      char inChar;
-        while(LBTServer.available() >0){
-          //Serial.write(LBTServer.read());
-          inChar = LBTServer.read();
-          switch(inChar){
-            case 'N':
-              
-              Serial.println("Forward");
-              break;
-            case 'E':
-              Serial.println("Right");
-              break;
-            case  'W':
-              Serial.println("Left");
-              break;
-            case 'S':
-              Serial.println("Backward");
-              break;
-          }
-        }
+      Serial.println("Connected");
     }
+  }
+  else
+  {
+    char inChar;
+    while (LBTServer.available() > 0) {
+      //Serial.write(LBTServer.read());
+      inChar = LBTServer.read();
+      switch (inChar) {
+        case 'N':
+          Serial.println("Forward");
+          leftMotor(1023, HIGH, LOW);
+          rightMotor(1023, LOW, HIGH);
+          delay(20);
+          break;
+        case 'E':
+          Serial.println("Right");
+          leftMotor(1023, HIGH, LOW);
+          rightMotor(1023, HIGH, LOW);
+          delay(20);
+          break;
+        case  'W':
+          Serial.println("Left");
+          leftMotor(1023, LOW, HIGH);
+          rightMotor(1023, LOW, HIGH);
+          delay(20);
+          break;
+        case 'S':
+          Serial.println("Backward");
+          leftMotor(1023, LOW, HIGH);
+          rightMotor(1023, HIGH, LOW);
+          delay(20);
+          break;
+        case '1':
+          Serial.println("RightForward");
+          leftMotor(1023, HIGH, LOW);
+          rightMotor(400, LOW, HIGH);
+          delay(20);
+          break;
+        case '2':
+          Serial.println("LeftForward");
+          leftMotor(400, HIGH, LOW);
+          rightMotor(1023, LOW, HIGH);
+          delay(20);
+          break;
+        case '3':
+          Serial.println("LeftBackward");
+          leftMotor(400, LOW, HIGH);
+          rightMotor(1023, HIGH, LOW);
+          delay(20);
+          break;
+        case '4':
+          Serial.println("RightBackward");
+          leftMotor(1023, LOW, HIGH);
+          rightMotor(400, HIGH, LOW);
+          delay(20);
+          break;
+      }
+    }
+    leftMotor(0, LOW, LOW);
+    rightMotor(0, LOW, LOW);
+  }
 }
---
+++ Motor.ino
@@ -0,0 +1,31 @@
+#define  PWMA  3
+#define  REVA  2
+#define  FORA  4
+#define  PWMB  9
+#define  REVB  8
+#define  FORB  10
+#define  RELAY  13
+
+void initialMotor() {
+  pinMode(RELAY, OUTPUT);
+  pinMode(REVA, OUTPUT);
+  pinMode(FORA, OUTPUT);
+  pinMode(REVB, OUTPUT);
+  pinMode(FORB, OUTPUT);
+  digitalWrite(RELAY, LOW);
+  digitalWrite(REVA, LOW);
+  digitalWrite(FORA, LOW);
+  digitalWrite(REVB, LOW);
+  digitalWrite(FORB, LOW);
+}
+void leftMotor(unsigned int leftMotor, boolean FOR,boolean REV) {
+  analogWrite(PWMA, leftMotor);
+  digitalWrite(FORA, FOR);
+  digitalWrite(REVA, REV);
+}
+void rightMotor(unsigned int rightMotor, boolean FOR,boolean REV) {
+  analogWrite(PWMB, rightMotor);
+  digitalWrite(FORB, FOR);
+  digitalWrite(REVB, REV);
+}
+
